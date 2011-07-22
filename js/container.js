687d88730fa8581f563b215d1977b1aef89aa16e
Gaston Ramos <ramos.gaston@gmail.com>
2011-07-22 12:10:17 -0300

    - added bind_update_fields

--- js/container.js
+++ js/container.js
@@ -1,10 +1,12 @@
 var Container = Backbone.Model.extend({
-  defaults: {name: "My Container"}
+    defaults: {name: "My Container", capacity: 10, limit:20}
 });
 
 var ContainerView = Backbone.View.extend({
 
   template: $('#container-template').html(),
+
+  className: "container",
 
   initialize: function() {
     _.bindAll(this, "render");
@@ -13,9 +15,34 @@ var ContainerView = Backbone.View.extend({
     this.model.view = this;
   },
 
+  /*
+    We need to update the model attrs if the user change
+    values from the input fileds.
+  */
+  bind_update_fields: function() {
+    var fields = $('#container-a').find('.field');
+    var events = {};
+    _.each(fields, function(f) {
+      events["change input." + f.name] = "update_" + f.name
+    });
+    this.events = events;
+    return events;
+  },
+
+  update_limit: function(e) {
+    console.log(e);
+  },
+
+  update_capacity: function(e) {
+
+    console.log(e);
+    // this.model.set({capacity: e.targe.value});
+  },
+
   render: function() {
-    rendered_data =  $.tmpl( this.template, this.model.toJSON() )
+    var rendered_data =  $.tmpl( this.template, this.model.toJSON() );
     $('#container-a').html(rendered_data);
+    this.bind_update_fields();
     return this;
   }
 
@@ -23,4 +50,5 @@ var ContainerView = Backbone.View.extend({
 
 window.container_view = new ContainerView;
 window.container_view.render();
+
 
