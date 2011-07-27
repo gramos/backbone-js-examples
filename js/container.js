84646fc4f6c9aa395c70d448f0252c90f892e6c6
Gaston Ramos <ramos.gaston@gmail.com>
2011-07-27 13:14:03 -0300

    - added method update_field in ContainerView - generate events with a function

--- js/container.js
+++ js/container.js
@@ -9,43 +9,38 @@ var ContainerView = Backbone.View.extend({
   // className: "container",
   id: "ratio-container",
 
-
   initialize: function() {
-      _.bindAll(this, "render", "bind_update_fields");
+    _.bindAll(this, "render");
     this.model = new Container;
     this.model.bind('change', this.render);
     this.model.view = this;
   },
 
+  events: function() {
+    var fields = ['capacity', 'limit']
+    var _events = {}
+    _.each(fields, function(f) {
+      _events["change input." + f] = "update_field"
+    });
+    return _events;
+  }(),
+
   /*
     We need to update the model attrs if the user change
     values from the input fileds.
   */
-  bind_update_fields: function() {
-    var view   = this;
-    var fields = $('#container-a').find('.field');
-    var events = {};
-    _.each(fields, function(f) {
-      events["change input." + f.name] = "update_" + f.name
-    });
-    view.events = {"change input.capacity": "update_capacity"};
+  update_field: function(e) {
+    var o = {}
+    o[e.currentTarget.name] = e.target.value;
+    console.log(o);
+    this.model.set(o);
+    console.log(this.model.get(e.currentTarget.name));
   },
-
-  update_limit: function(e) {
-    console.log(e);
-  },
-
-  update_capacity: function(e) {
-    alert(e.target.value);
-    this.model.set({capacity: e.target.value});
-  },
-
-  events: {"change input.capacity": "update_capacity"},
 
   render: function() {
     var rendered_data =  $.tmpl( this.template, this.model.toJSON() );
-    // this.bind_update_fields();
     $(this.el).html(rendered_data);
+    // this.bind_update_fields();
     return this;
   }
 
